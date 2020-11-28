2f69257a6f606743d5873715829e30dee37fb1cf
Emerson Pinheiro <e_pms@hotmail.com>
2020-11-28 12:40:17 -0300

    Added core functionality

--- App.tsx
+++ App.tsx
@@ -1,118 +1,213 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
+import React, {useCallback, useState, useEffect} from 'react';
 import {
-  SafeAreaView,
+  Pressable,
   StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
+  TextInput,
+  View,
+  Keyboard,
+  Alert,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const DIGITS_LIMIT = 8;
 
-declare const global: { HermesInternal: null | {} };
+interface IInsertPressabeProps {
+  title: string;
+  onPress: () => void;
+}
+
+const InsertPressable: React.FC<IInsertPressabeProps> = ({title, onPress}) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      android_ripple={{borderless: false, radius: 120}}
+      style={styles.pressable}
+    >
+      <View style={{flex: 1}}>
+        <Text style={styles.pressableTitle}>{title}</Text>
+      </View>
+    </Pressable>
+  );
+};
 
 const App = () => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [resultValue, setResultValue] = useState<string>('');
+  const [digitsLimitEnabled, setDigitsLimitEnabled] = useState<boolean>(true);
+  const [
+    showPressablesContainer,
+    setShowPressablesContainer,
+  ] = useState<boolean>(true);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => {
+        setShowPressablesContainer(false);
+      }
+    );
+
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        setShowPressablesContainer(true);
+      }
+    );
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+
+  const onInputChangeText = useCallback(
+    (text: string) => {
+      if (digitsLimitEnabled && inputValue.length <= DIGITS_LIMIT) {
+        const isValid = !!text.match(/(0|1)/gm);
+
+        if (isValid || !text.length) {
+          setInputValue(text);
+        } else {
+          Alert.alert('Please, enter 0 or 1 digit');
+        }
+      }
+    },
+    [digitsLimitEnabled, inputValue]
+  );
+
+  const onPressInsert = useCallback(
+    (value: string) => {
+      if (inputValue.length <= 8) {
+        setInputValue((iv) => iv + value);
+      }
+    },
+    [inputValue]
+  );
+
+  const convert = useCallback(() => {
+    let result: number = 0;
+
+    let pow = 0;
+    for (let i = inputValue.length - 1; i >= 0; i--) {
+      result += +inputValue[i] * 2 ** pow;
+      pow++;
+    }
+
+    setResultValue(result.toString());
+  }, [inputValue]);
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}
+    <View style={{flex: 1}}>
+      <View style={styles.resultBoardContainer}>
+        <View
+          style={{
+            flex: 1,
+            paddingBottom: 24,
+          }}
         >
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+          <Text style={{flex: 1}}>INPUT</Text>
+          <TextInput
+            style={{
+              height: 64,
+              fontSize: 34,
+              fontWeight: 'bold',
+              borderColor: '#E9C46A',
+              borderRadius: 10,
+              borderWidth: 2,
+            }}
+            keyboardType="numeric"
+            onChangeText={onInputChangeText}
+            value={inputValue}
+            maxLength={digitsLimitEnabled ? DIGITS_LIMIT : undefined}
+          />
+        </View>
+        <View style={{flex: 1}}>
+          <Text style={{flex: 1}}>RESULT</Text>
+          <Text
+            style={{
+              height: 64,
+              fontSize: 34,
+              fontWeight: 'bold',
+              borderColor: '#E9C46A',
+              borderRadius: 10,
+              borderWidth: 2,
+              textAlignVertical: 'center',
+              paddingLeft: 5,
+            }}
+          >
+            {resultValue}
+          </Text>
+        </View>
+      </View>
+      {showPressablesContainer && (
+        <View style={styles.pressablesContainer}>
+          <View style={styles.insertPressablesContainer}>
+            <InsertPressable
+              title="0"
+              onPress={() => onPressInsert('0')}
+            ></InsertPressable>
+            <InsertPressable
+              title="1"
+              onPress={() => onPressInsert('1')}
+            ></InsertPressable>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+          <View style={styles.convertPressableContainer}>
+            <Pressable
+              style={styles.convertPressable}
+              android_ripple={{borderless: false, radius: 350}}
+              onPress={convert}
+            >
+              <Text style={styles.pressableTitle}>CONVERT!</Text>
+            </Pressable>
+          </View>
+        </View>
+      )}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  resultBoardContainer: {
+    flex: 1,
+    backgroundColor: '#f5e1a2',
+    padding: 24,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  insertPressablesContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 24,
   },
-  body: {
-    backgroundColor: Colors.white,
+  pressable: {
+    width: '45%',
+    height: '100%',
+    elevation: 4,
+    backgroundColor: '#264653',
+    borderRadius: 10,
   },
-  sectionContainer: {
-    marginTop: 32,
+  pressablesContainer: {
+    flex: 1,
+    elevation: 4,
+    backgroundColor: '#e0cf96',
+  },
+  pressableTitle: {
+    flex: 1,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  convertPressable: {
+    height: '80%',
+    width: '100%',
+    backgroundColor: '#2A9D8F',
+    borderRadius: 10,
+    elevation: 4,
+  },
+  convertPressableContainer: {
+    flex: 1,
     paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
 
